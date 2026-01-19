7a67db0ab670c56e1560995eb9442660bc1e0bc1
gtebrean <99179176+gtebrean@users.noreply.github.com>
2026-01-19 11:55:43 +0200

    data debug -wait
    Signed-off-by: gtebrean <99179176+gtebrean@users.noreply.github.com>

--- src/main/kotlin/org/web3j/container/besu/BesuContainer.kt
+++ src/main/kotlin/org/web3j/container/besu/BesuContainer.kt
@@ -39,5 +39,4 @@ class BesuContainer(
 //            .forStatusCode(200).forPort(8545)
         Wait
             .forLogMessage(".*JsonRpcHttpService.*started.*|.*RPC endpoint.*listening.*|.*Starting.*JSON-RPC.*service.*", 1)
-            .withStartupTimeout(java.time.Duration.ofMinutes(5))
 }
